257873668c3e7ff8306566fa96f8268b354c03b9
Irisgaby30 <irisgabrielyoliveira985@gmail.com>
2026-04-08 11:39:26 -0300

    Create exercicio5.java

---
+++ Aula06/exercicio5.java
@@ -0,0 +1,11 @@
+public class exercicio5 {
+  public static void main(String[]args){
+    String palavra = "Computação";
+    char primeiro = palavra.charAt(0);
+    char terceiro = palavra.charAt(2);
+    char ultimo = palavra.charAt(palavra.length()-1);
+    System.out.println("Primeiro: + primeiro");
+    System.out.println("terceiro: + terceiro");
+    System.out.println("ultimo: + ultimo");
+  }
+}
