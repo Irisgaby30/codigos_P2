403934ea6d1ce092c92cbca6de8b6ce8f29cf364
Irisgaby30 <irisgabrielyoliveira985@gmail.com>
2026-04-08 11:11:00 -0300

    Create exercicio2.java

---
+++ Aula06/exercicio2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class exercicio2 {
+  public static void maid(String[]args){
+    Scanner leitor = new Scanner(System.in);
+    System.out.print("Digite a primeira palavra:");
+    String p1 = leitor.nextLine();
+    System.out.print("Digite a segunda palavra:");
+    String p2 = leitor.nextLine();
+    if (p1.equalsIngoreCase(p2)) {
+      System.out.println("As palavras sao iguais (ingorando maiusculas/minusculas).");
+    } else {
+      System.out.println("As palavras sao diferentes.");
+    }
+    leitor.close();
+  }
+}
