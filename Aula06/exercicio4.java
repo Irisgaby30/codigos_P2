9ceee1ec4c71aeaed3ddf70a257b2f22605cb285
Irisgaby30 <irisgabrielyoliveira985@gmail.com>
2026-04-08 11:30:02 -0300

    Create exercicio4.java

---
+++ Aula06/exercicio4.java
@@ -0,0 +1,9 @@
+public class exercicio4 {
+  public static void main(String[]args){
+  String palavra = "Programação";
+    String parte1 = palavra.substring(0,8);
+    String parte2 = palavra.substring(7);
+    System.out.println("parte1");
+    System.out.println("parte2");
+  }
+}
