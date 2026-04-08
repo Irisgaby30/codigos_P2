71c866fd822feb75b47e84ee909fedcde4c21a1b
Irisgaby30 <irisgabrielyoliveira985@gmail.com>
2026-04-08 11:21:58 -0300

    Update exercicio1.java

--- Aula06/exercicio1.java
+++ Aula06/exercicio1.java
@@ -1,6 +1,6 @@
  import java.util.Scanner;
 public class exercicio1{
-public static void maid (String[] args) {
+public static void main (String[] args) {
   Scanner leitor = new Scanner (System.in);
   System.out.print("Digite o primeiro nome:");
   String nome1 = leitor.nextLine();
