b7fc15fd8d4fbb100bf88a4b4e26f1b08b10f5c4
Irisgaby30 <irisgabrielyoliveira985@gmail.com>
2026-04-08 12:02:01 -0300

    Update exercicio6.java

--- Aula06/exercicio6.java
+++ Aula06/exercicio6.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class exercicio6 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite uma frase");
+        String frase = leitor.nextLine();
+        System.out.print("Digite a palavra para buscar:");
+        String palavra = leitor.nextLine();
+        int posição = frase.indexOf(palavra);
+        if (posição!= -1) {
+        System.out.println("A palavras aparece na frase na posição: + posição ");
+        } else {
+            System.out.println("A palavra não foi encontarada (Retorno:-1)");
+        }
+        leitor.close();
 
+    }
+}
