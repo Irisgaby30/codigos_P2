bd71f741008c454f83e02b28b435b619bae8f202
Irisgaby30 <irisgabrielyoliveira985@gmail.com>
2026-04-08 11:21:32 -0300

    Create exercicio3.java

---
+++ Aula06/exercicio3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class exercicio3 {
+  public static void main(String[]args){
+    Scanner leitor = new Scanner(System.in);
+    System.out.print("Digite uma palavra:");
+    String palavra = leitor.nextLine();
+    int quantidade = palavra.length();
+    System.out.println("A palavra'"+ palavra + "'possui" + quantidade + "caracteres");
+    leitor.close();
+  }
+}
